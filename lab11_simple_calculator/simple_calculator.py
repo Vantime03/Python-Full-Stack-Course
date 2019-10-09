72f525e0435972e061309783ce67035aa63c5cc0
Vantime03 <van.b.luong@hotmail.com>
2019-10-08 20:27:20 -0700

    simple calculator :fire:

--- lab11_simple_calculator/simple_calculator.py
+++ lab11_simple_calculator/simple_calculator.py
@@ -1,22 +1,19 @@
 #simple calculator app
+import string
 
-import operator
-
-dic_operator = {
-    "+": operator.add,
-    "-": operator.sub,
-    "*": operator.mul,
-    "/": operator.truediv
-} #https://docs.python.org/3/library/operator.html
-
-def calculate(first_numb, operation_input, second_numb):
-    print(f"{first_numb} {operation_input} {second_numb} = {dic_operator[operation_input](first_numb, second_numb)}")
-
+def verify_injection(user_input):
+    for n in user_input:
+        if n in string.ascii_lowercase or n in string.ascii_uppercase:
+            return True
+        else:
+            return False
 def main():
     print("Welcome to Simple calculator!\n")
-    operation_input = input("Enter an operation you wish to perform (+, -, *, /): ")
-    first_numb = float(input("Enter the first number: "))
-    second_numb = float(input("Enter the second number: "))
-    calculate(first_numb, operation_input, second_numb)
+    expression = input("Enter an arithmetic expression (e.g. '2 + 3 + 5'): ")
+    injection_boolean = verify_injection(expression)
+    if injection_boolean is False:
+        print(eval(expression))
+    else:
+        print("THAT IS AN INVALID expression!!!! NO INJECTION ALLOW!")
 
 main()
