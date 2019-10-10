4c8a1532976bb6c350bc273dc5d691b325da2c3a
Vantime03 <van.b.luong@hotmail.com>
2019-10-09 18:01:59 -0700

    guess a number version 4

--- lab12_guess_number/guess_a_number.py
+++ lab12_guess_number/guess_a_number.py
@@ -2,27 +2,28 @@
 Lab 12: Guess the Number
 Let's play 'Guess the Number'. The computer will guess a random int between 1 and 10. The user will then try to guess the number, and the program will tell them whether they're right or wrong.
 '''
-#version 3 - tell user wheter they guess high or low
+#version 4 - tell user wheter the current guess is closer to the previous guess
 import random
 
 #this will evaluate high and low
-def evaluate_high_low(guess_number, pc_number):
-    if guess_number - pc_number >= 4:
-        print("Too high!")
-    if pc_number - guess_number >= 4:
-        print("Too low!")
-
+def evaluate_high_low(current_guess, last_guess, pc_number):
+    if abs(current_guess-pc_number) < abs(last_guess-pc_number):
+        print("Your current guess is closer to the target compared to the previous guess!")
+    else:
+        print("Your current guess is farther to the target compared to the previous guess!")
 
 def guess(pc_number):
+    current_guess = 0
+    last_guess = 0
     guess_wrong = True
     while guess_wrong:
-        guess_number = int(input("Enter a number to guess: "))
-        if guess_number == pc_number:
-            print(f"Your guess is {guess_number} and computer's number is {pc_number}. You are correct!!!")
-            guess_wrong = False
+        current_guess = int(input("Enter a number to guess: "))
+        if current_guess == pc_number:
+            print(f"Your guess is {current_guess} and computer's number is {pc_number}. You are correct!!!")
+            break
         print("Try again!")
-        evaluate_high_low(guess_number, pc_number)
-
+        evaluate_high_low(current_guess, last_guess, pc_number)
+        last_guess = current_guess
 def main():
     print("Wecome to Guess-A-Number!!!\n")
     pc_number = random.randint(1,10)
