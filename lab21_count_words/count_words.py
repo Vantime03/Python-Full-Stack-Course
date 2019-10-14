7220ae4da3b83a8b4d4ae6e87a630086c8e07283
Vantime03 <van.b.luong@hotmail.com>
2019-10-14 12:53:53 -0700

    count words version 2

--- lab21_count_words/count_words.py
+++ lab21_count_words/count_words.py
@@ -19,34 +19,51 @@ import string
 book= open("C:\\Users\\vanbinhluong\\Desktop\\PythonFullStack\\lab21_count_words\\VERSES_BY_H_BELLOC.txt", encoding="utf-8")
 book_dict = {}
 
+def strip_punctuation(book_string):
+    for n in string.punctuation:
+        book_string = book_string.replace(n, "")
+    return book_string
+
+def process_dictionary(book_string):
+    word = ""
+    first_word = ""
+    second_word = ""
+    for i in range(len(book_string)):
+        if book_string[i] != " ": 
+            word += book_string[i]
+        elif book_string[i] == " " and first_word == "":
+            first_word = word + " "    
+            word = ""
+        elif book_string[i] != " " and first_word != "" and second_word == "":
+            word += book_string[i]
+        elif book_string[i] == " " and first_word != "" and second_word == "":
+            second_word = word + " "
+            word = ""
+            if (first_word + second_word) in book_dict:
+                count = book_dict.get(first_word + second_word) + 1
+                book_dict[first_word+second_word] = count
+            else:
+                book_dict[first_word+second_word] = 1
+            first_word = second_word
+            second_word = ""
+
+
+
+def finding_top_ten():
+    words = list(book_dict.items()) # return a list of tuples
+    words.sort(key=lambda tup: tup[1], reverse=True)
+    print("\nHere is the top 10 words in Verse:\n")
+    for i in range(min(10, len(words))):
+        print(words[i])
+    
 def main():
     print("Welcom to Word Counts!")
     booktxt = book.read()
     book.close()
-    book_string = booktxt.lower()
-
-    #strip all punctuations
-    for n in string.punctuation:
-        book_string = book_string.replace(n, "")
-    
-    # this will put each unique word into a dictionary start increment
-    word = ""
-    for n in book_string: 
-        if n != " ":
-            word += n
-        elif n == " ":
-            if word in book_dict: # verify of word is in the existing dict
-                count = book_dict.get(word) + 1
-                book_dict[word] = count
-                word = ""
-            else: 
-                book_dict[word] = 1
-                word = ""
-
-    words = list(book_dict.items()) # return a list of tuples
-    words.sort(key=lambda tup: tup[1], reverse=True)
-    for i in range(min(10, len(words))):
-        print(words[i])
+    book_string = booktxt.lower() # lower cap words
+    book_string = strip_punctuation(book_string) #strip all punctuations
+    process_dictionary(book_string) # this will put each unique word into a dictionary start increment
+    finding_top_ten()
 
 main()
 
