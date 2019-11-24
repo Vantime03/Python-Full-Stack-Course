46e54cbad6bb57643c9455835016bf3f6ff2a1e5
Vantime03 <54970162+Vantime03@users.noreply.github.com>
2019-11-24 14:15:24 -0800

    to do list & capstone proposal

--- Javascript/Lab 2: Todo List/main.js
+++ Javascript/Lab 2: Todo List/main.js
@@ -1,3 +1,5 @@
+let count = 0
+
 document.querySelector('.add').addEventListener('click', event => {
     let list = document.querySelector('.list')
     let userInput = document.querySelector('.new-item')
@@ -5,9 +7,29 @@ document.querySelector('.add').addEventListener('click', event => {
 
     newItem.appendChild(document.createTextNode(userInput.value))
     newItem.setAttribute('class','item-list')
+    newItem.setAttribute('id', `item${count}`)
+    newItem.setAttribute('oncontextmenu', `dblClick_remove('item${count}')`)
+    newItem.setAttribute('onclick', `strike_through('${userInput.value}', 'item${count}')`)
+    count += 1
     list.appendChild(newItem)
 })
 
+
+
+function strike_through (userInput, html_element) {
+    
+    let result = userInput.strike()
+    document.getElementById(`${html_element}`).innerHTML = result
+    document.getElementById(`${html_element}`).style.color = 'green'
+
+}
+
+function dblClick_remove (html_element) {
+    let item = document.getElementById(`${html_element}`)
+    if (confirm('Are you sure you want to remove this task?')) {
+        item.remove()
+    } 
+}
 
 document.querySelector('.remove').addEventListener('click', event => {
     let list = document.querySelector('.list')
