e4bd314776964de8882bb7fbeaf3fdc26c67d241
Vantime03 <54970162+Vantime03@users.noreply.github.com>
2019-11-25 18:09:47 -0800

    capstone write up

--- Capstone/proposal.md
+++ Capstone/proposal.md
@@ -1,18 +1,29 @@
 # Peer-to-Peer Tool Lending
 
+### Project Overview
 
+The purpose of this project is to create a peer-to-peer tool lending site. This site will allow users to lend their tools to others at a very low cost.
 
+What's the benefit of peer-to-peer tool lending program? Here are the benefits: 
+    (1) Cost of renting a tool can be lower comparing to other local tool rental companies.
+    (2) Tool owners can make extra money from renting out their tools. 
+    (3) You can still lend tools to your friends or family with this system. With this system, you have them accountable for what they're borrowing. 
 
-### Functional Requirements:
+### Project Scope
 
-This site will have the following functional requirments: 
+The scope of this project will consist of the following functional requirments: 
 
 1. Allow new users (lenders or borrowers) to register accounts. 
 2. Allow user to Logon and Logoff. 
-3. Allow borrowers to search equipment from the Equipment database for renting. 
-4. Allow lenders to upload equipment to equipment database for leasing. 
-. database fEquipment database table that shows a list of equipment for rent
-. Transaction database table that shows all historical transactions
-. User database table that contain user's profile information
+3. Allow borrowers to search equipment from the equipment database for renting. 
+4. Allow lenders to upload equipment to the equipment database for leasing. Provide suggested leasing price based on the existing market
+5. Create Equipment database table that shows a list of equipment for rent.
+6. Create Transaction database table that shows all current and historical transactions.
+7. Create User database table that contain user's profile information.
 
-![sample](sample.png)
+<!-- ![sample](sample.png) -->
+
+
+
+
+
--- Javascript/Lab 2: Todo List/main.js
+++ Javascript/Lab 2: Todo List/main.js
@@ -3,37 +3,61 @@ let count = 0
 document.querySelector('.add').addEventListener('click', event => {
     let list = document.querySelector('.list')
     let userInput = document.querySelector('.new-item')
+    
     let newItem = document.createElement('li')
-
-    newItem.appendChild(document.createTextNode(userInput.value))
+    newItem.appendChild(document.createTextNode(`${userInput.value}`))  
     newItem.setAttribute('class','item-list')
     newItem.setAttribute('id', `item${count}`)
-    newItem.setAttribute('oncontextmenu', `dblClick_remove('item${count}')`)
-    newItem.setAttribute('onclick', `strike_through('${userInput.value}', 'item${count}')`)
-    count += 1
+    newItem.setAttribute('name', 'todoitem')
+    // newItem.setAttribute('oncontextmenu', `dblClick_remove('item${count}')`)
+    // newItem.setAttribute('onclick', `strike_through('${userInput.value}', 'item${count}')`)
     list.appendChild(newItem)
+
+    let checkbox = document.createElement('input')
+    checkbox.type = "checkbox"
+    checkbox.id = `item_checkbox${count}`  
+    checkbox.setAttribute('onclick', `checked_strike('${count}')`)
+    newItem.appendChild(checkbox)
+    count += 1
 })
 
+function checked_strike (item){
+    let selected_box = document.getElementById(`item_checkbox${item}`)
+    let value = document.getElementById(`item${item}`)
+    if (selected_box.checked == true) {
+        value.style.textDecoration = 'line-through'
+        value.style.color = 'green'
 
-
-function strike_through (userInput, html_element) {
-    
-    let result = userInput.strike()
-    document.getElementById(`${html_element}`).innerHTML = result
-    document.getElementById(`${html_element}`).style.color = 'green'
-
+    }
 }
 
-function dblClick_remove (html_element) {
-    let item = document.getElementById(`${html_element}`)
-    if (confirm('Are you sure you want to remove this task?')) {
-        item.remove()
-    } 
-}
+// function strike_through (userInput, html_element) {
+//     let result = userInput.strike()
+//     document.getElementById(`${html_element}`).innerHTML = result
+//     document.getElementById(`${html_element}`).style.color = 'green'
+// }
+
+// function dblClick_remove (html_element) {
+//     let item = document.getElementById(`${html_element}`)
+//     if (confirm('Are you sure you want to remove this task?')) {
+//         item.remove()
+//     } 
+// }
 
 document.querySelector('.remove').addEventListener('click', event => {
     let list = document.querySelector('.list')
     let items = document.querySelectorAll('.item-list')
     list.removeChild(items[0])
+    let elements = document.getElementsByName('todoitem')
+    
+    // for (let i = 0; i < elements.length; i++) {
+    //     let checkbox_item = document.getElementById(`item_checkbox${i}`)
+    //     let text_item = document.getElementById(`item${i}`)
+    //     if (checkbox_item.checked == true) {
+    //         checkbox_item.remove()
+    //         text_item.remove()
+    //     }
+    // }
+    
 })
 
