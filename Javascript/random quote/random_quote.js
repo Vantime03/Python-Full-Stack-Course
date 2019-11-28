58044331eec7bc6e0c1046bf3650647380926ec1
Vantime03 <54970162+Vantime03@users.noreply.github.com>
2019-11-27 20:54:11 -0800

    random quote v2 with secret hidden api

--- Javascript/random quote/random_quote.js
+++ Javascript/random quote/random_quote.js
@@ -1,11 +1,68 @@
+let Quote = []
+let Tag = []
+
 function get_api () {
-	const url = 'https://favqs.com/api/qotd'
-	axios.get(url)
-	.then(function (request) {
-        let quote = request.data.quote.body
-        document.getElementById("quote").innerHTML = quote 
+    const url = 'https://favqs.com/api/quotes'
+
+    axios.get(url, {
+        headers: {
+            'authorization': `Token token=${key}`
+        }
+    }) 
+	// .then(function (request) {
+    //     console.log(request.data);
+    //     // let quote = request.data.quote.body
+    //     // document.getElementById("quote").innerHTML = quote 
+    // })
+    .then(function (request) {
+        console.log(request.data)
+        console.log(request.data.quotes[3].tags[0])
+        let tag = []
+        let quote = []
+        for (i=0; i < (request.data.quotes.length); i++){
+            // tag.push(request.data.quotes[i].tags[0])
+            let len = (request.data.quotes[i].tags).length
+            for (a=0; a<len; a++){
+                quote.push(request.data.quotes[i].body)
+                let Is_tag_in_array = tag.includes(request.data.quotes[i].tags[a])
+                if (Is_tag_in_array == false) {
+                    tag.push(request.data.quotes[i].tags[a])
+                }
+            }
+        }
+        let string = 'Filter Keywords: '
+        for (i=0;i<tag.length;i++){string = string + tag[i] + ', '; if (i == tag.length-1){string += tag[i]}}
+        document.getElementById("filter_list").innerHTML = string
+        Tag = tag
+        Quote = quote
     })
-	.catch(error => console.log(error))
+    .catch(error => console.log(error))
+}
+
+document.getElementById("search_by_tag").addEventListener('click', () => {
+    search_quote();
+})
+
+function search_quote() {
+    let search_value = document.getElementById("tag_textbox").value
+    let Is_searchValue_in_Tag_array = Tag.includes(search_value)
+    let result = `quotes with '${search_value}': <br><br>`
+    let arr = []
+    if (Is_searchValue_in_Tag_array == true){
+        for(i=0;i<Quote.length;i++){
+            let n = Quote[i].includes(search_value)
+            if (n == true) {
+                if (arr.includes(Quote[i]) == false){
+                    arr.push(Quote[i])
+                }
+            }
+        }
+    }
+    for (i=0;i<arr.length;i++){
+        result += arr[i] + "<br><br>"
+    }
+    document.getElementById("result").innerHTML = result
+
 }
 
 get_api()
