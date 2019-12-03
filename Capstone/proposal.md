aaad42a6d7216d01663afd146c40ad833c1acf7a
Vantime03 <54970162+Vantime03@users.noreply.github.com>
2019-12-02 20:53:44 -0800

    adding leaflet mapping and to Weather API

--- Capstone/proposal.md
+++ Capstone/proposal.md
@@ -9,6 +9,8 @@ What's the benefit of peer-to-peer tool lending program? Here are the benefits:
     (2) Tool owners can make extra money from renting out their tools. 
     (3) You can still lend tools to your friends or family with this system. With this system, you have them accountable for what they're borrowing. 
 
+### Tools I will use: Materialize CSS Framework, NodeJS, SQL Lite, Python (please let me know if I need to know more tools)
+
 ## Project Scope
 
 The scope of this project will consist of the following functional requirments: 
@@ -17,11 +19,11 @@ The scope of this project will consist of the following functional requirments:
 2. Allow users (borrowers or lenders) to Logon and Logoff. 
 3. Allow borrowers to search equipment from the equipment database for renting. 
 4. Allow borrowers to request an equipment for rent. 
-4. Allow lenders to upload equipment to the equipment database for leasing. Provide suggested leasing price based on the existing market
-5. Allow lenders to accept the rental request. 
-5. Create Equipment database table that shows a list of equipment for rent.
-6. Create Transaction database table that shows all current and historical transactions.
-7. Create User database table that contain user's profile information.
+5. Allow lenders to upload equipment to the equipment database for leasing. Provide suggested leasing price based on the existing market
+6. Allow lenders to accept the rental request. 
+7. Create Equipment database table that shows a list of equipment for rent.
+8. Create Transaction database table that shows all current and historical transactions.
+9. Create User database table that contain user's profile information.
 
 ## Data model
 
--- Javascript/Lab10: Weather API/WeatherAPI.js
+++ Javascript/Lab10: Weather API/WeatherAPI.js
@@ -1,17 +1,58 @@
 document.getElementById('search').addEventListener('click', () => {
-    let zipcode = document.getElementById('zipcode').value 
+    let zipcode = document.getElementById('zipcode').value
     const url = `https://api.openweathermap.org/data/2.5/weather?zip=${zipcode}&units=imperial&appid=${key}`
 
     axios.get(url)
-        
-    .then (function (response) {
-        // console.log(response.data);
-        document.getElementById('currentTemperature').innerHTML = `Current Temperature: ${response.data.main.temp}`
-        document.getElementById('description').innerHTML = `Current Condition: ${response.data.weather[0].description}`
-        document.getElementById('windSpeed').innerHTML = `Current Condition: ${response.data.wind.speed} miles per hour`
 
-        console.log(response.data);
-    })
-    .catch(error => console.log(error))
+        .then(function (response) {
+
+            document.getElementById('mapid').remove()
+
+            // display temperature
+            let temp = document.getElementById('currentTemperature')
+            // temp.setAttribute('class', 'card light-blue darken-1' )
+            temp.innerHTML = `Current Temperature: ${response.data.main.temp} &#8457`
+
+            let desc = document.getElementById('description')
+            // desc.setAttribute('class', 'card light-blue darken-1' )
+            desc.innerHTML = `Current Condition: ${response.data.weather[0].description}`
+
+            let windspeed = document.getElementById('windSpeed')
+            // windspeed.setAttribute('class', 'card light-blue darken-1' )
+            document.getElementById('windSpeed').innerHTML = `Wind Speed: ${response.data.wind.speed} Mi./Hr.`
+            console.log(response.data);
+
+
+            let map_placement = document.getElementById('map_placement')
+            let map = document.createElement('div')
+            map.setAttribute('id', 'mapid')
+            map.setAttribute('class', 'col s12')
+            map_placement.appendChild(map)
+
+
+
+            //getting the coordinate
+            let lat = response.data.coord.lat
+            let long = response.data.coord.lon
+
+
+            //leaflet mapping
+            let mymap = L.map('mapid').setView([lat, long], 13);
+            const url_map = `https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=${key_map}`
+            
+            //marker for mapping
+            var marker = L.marker([lat, long]).addTo(mymap);
+            marker.bindPopup(`<b>Welcome to ${response.data.name}!</b><br>`).openPopup();
+
+            L.tileLayer(url_map, {
+                attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+                maxZoom: 18,
+                id: 'mapbox/streets-v11',
+                accessToken: 'your.mapbox.access.token'
+            }).addTo(mymap);
+
+        })
+        .catch(error => console.log(error))
+
 })
 
