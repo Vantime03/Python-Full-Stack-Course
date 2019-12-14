bd025f643fabbeac129aa1167b9de00824c319f2
Vantime03 <54970162+Vantime03@users.noreply.github.com>
2019-12-13 20:17:49 -0800

    setup hello world with two models.

--- Django/Hello_World/say_hello/models.py
+++ Django/Hello_World/say_hello/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.conf import settings
+from django.utils import timezone
+
 
 # Create your models here.
+class  Borrower(models.Model):
+    firstname = models.CharField(max_length=100)
+    lastname = models.CharField(max_length=100)
+    mailing_address = models.CharField(max_length=200)
+    city = models.CharField(max_length=50)
+    state = models.CharField(max_length=2)
+    zip_code = models.CharField(max_length=5)
+
+    def __str__(self):
+        return self.firstname + " " + self.lastname
+
+class Equipment(models.Model):
+    modelnumber = models.CharField(max_length=50)
+    serialnumber = models.CharField(max_length=50)
+    ToolType = models.CharField(max_length=30)
+    borrowerid = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    rentcost = models.FloatField
+    availabe = models.BooleanField
+
+
+
