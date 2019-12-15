19c10f9abc9acb4fae95afae03ba56499fef88be
Vantime03 <54970162+Vantime03@users.noreply.github.com>
2019-12-14 19:48:36 -0800

    blog part 3 | playing with base template and block content and css

--- Django/Blog/blog/views.py
+++ Django/Blog/blog/views.py
@@ -1,8 +1,24 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
+posts = [
+    {
+        'author': 'Vantime',
+        'title': 'Blog Post 1',
+        'content': 'First post check-in: tutorial video #3',
+        'date_posted': 'December 14, 2019',
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post check-in: tutorial video #3 | test post ',
+        'date_posted': 'December 14, 2019',
+    }
+]
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html', {'title': 'About Us'})
