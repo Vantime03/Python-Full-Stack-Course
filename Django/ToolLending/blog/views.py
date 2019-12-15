2138cf348f5ef4dd7b3bf6b5f045761babfc1ed3
Vantime03 <54970162+Vantime03@users.noreply.github.com>
2019-12-14 23:14:28 -0800

    practice blog: tool lending blog | finished video 3

--- Django/ToolLending/blog/views.py
+++ Django/ToolLending/blog/views.py
@@ -18,11 +18,13 @@ posts = [
 ]
 
 def home(request):
+    return render(request, 'blog/home.html', {'title': 'Home'})
+
+def blog(request):
     context = {
         'posts': posts
     }
-
     return render(request, 'blog/blog.html', context)
 
 def about(request):
-    return render(request, 'blog/about.html')
+    return render(request, 'blog/about.html', {'title': 'About Us'})
