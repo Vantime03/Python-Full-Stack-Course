3c8c1df2396d0521a726d7b896f63664df3dd30e
Vantime03 <54970162+Vantime03@users.noreply.github.com>
2019-12-16 19:47:48 -0800

    project: Blog | progress: part 5 Database and migrations

--- Django/Blog/blog/views.py
+++ Django/Blog/blog/views.py
@@ -1,22 +1,9 @@
 from django.shortcuts import render
+from .models import Post
 
-posts = [
-    {
-        'author': 'Vantime',
-        'title': 'Blog Post 1',
-        'content': 'First post check-in: tutorial video #3',
-        'date_posted': 'December 14, 2019',
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second post check-in: tutorial video #3 | test post ',
-        'date_posted': 'December 14, 2019',
-    }
-]
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
