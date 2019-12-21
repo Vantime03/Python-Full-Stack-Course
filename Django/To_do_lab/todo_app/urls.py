5f3ef480111594c87895877859c59b6b30b43118
Vantime03 <54970162+Vantime03@users.noreply.github.com>
2019-12-21 10:40:55 -0800

    Complete todo list

--- Django/To_do_lab/todo_app/urls.py
+++ Django/To_do_lab/todo_app/urls.py
@@ -6,6 +6,8 @@ urlpatterns = [
     path('list/', views.list, name='list'),
     path('list/<int:id>', views.detail, name='detail'),
     path('create/', views.create, name='create'),
+    path('update/<int:id>', views.update, name='update'),
+    path('delete/<int:id>', views.delete, name='delete'),
 ]
 
 
--- Django/To_do_lab/todo_app/views.py
+++ Django/To_do_lab/todo_app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Todo
 from django.http import Http404
+# from django.utils.datastructures import MultiValueDictKeyError
 
 def home(request):
     return HttpResponse("This is Home Page")
@@ -11,6 +12,8 @@ def list(request):
         'Tasks': Todo.objects.all()
     }
     return render(request, 'todos/list.html', context)
+   
+
 
 def detail(request, id):
     try:
@@ -31,4 +34,36 @@ def create(request):
             status_field = True
         todo = Todo.objects.create(title = title_field, text = text_field, status = status_field)
         return redirect('list')
+
+def update(request, id):
+
+    if request.method == 'GET':
+        task = Todo.objects.get(pk=id)
+        return render(request, 'todos/update.html', {'task': task})
+        
+    elif request.method == 'POST':
+        title_field = request.POST['title']
+        text_field = request.POST['text']
+        
+        if request.POST['status'] == "False":
+            status_field = False
+        else:
+            status_field = True
+        
+        created_date_field = request.POST['created_at']
+
+        completed_date_field = request.POST['completed_at']
+        if completed_date_field == "":
+            completed_date_field = None
+
+        Todo.objects.filter(pk=id).update(title=title_field, text=text_field, status=status_field, created_at=created_date_field, completed_at=completed_date_field)
+        
+        return redirect('list')
+
+def delete(request, id):
+    task = Todo.objects.get(pk=id)
+    task_name = task.title
+    task.delete()
+    return render(request, 'todos/delete.html')
+
 
