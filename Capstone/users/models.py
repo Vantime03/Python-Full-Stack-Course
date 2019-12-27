4c433c62b788bd3518e2542942e3ce4634ec597b
Vantime03 <54970162+Vantime03@users.noreply.github.com>
2019-12-26 17:11:30 -0800

    create a profile model and associate it with build-in User model.

--- Capstone/users/models.py
+++ Capstone/users/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+    mailing_address = models.CharField(max_length=200)
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=100)
+    zip_code = models.CharField(max_length=10)
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
