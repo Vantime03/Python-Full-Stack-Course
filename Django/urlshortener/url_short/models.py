9ecdfa0407a4d003a49844f9fb16c30500bf7b7b
Vantime03 <54970162+Vantime03@users.noreply.github.com>
2019-12-31 17:51:46 -0800

    url shortener lab: work in progress

--- Django/urlshortener/url_short/models.py
+++ Django/urlshortener/url_short/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Todo(models.Model):
+    code = models.CharField(max_length=6)
+    url = models.TextField(max_length=300)
+
+    def __str__(self):
+            return self.url
+
+    class Meta:
+        ordering = ['id', 'url']
--- Django/urlshortener/url_short/views.py
+++ Django/urlshortener/url_short/views.py
@@ -1,5 +1,45 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, Http404
+from .models import Todo
+import random
+import string
+
+###This section will create a random generate URL
+def generate_short_url():
+    charlist = string.ascii_letters + string.digits
+    short_url = ''
+    for i in range (6):
+        char = random.choice(charlist)
+        short_url += char
+    return short_url
 
 def home(request):
-    return render(request, 'main.html')
+    context = {
+        'short_url': Todo.objects.all()
+    }
+    return render(request, 'main.html', context)
+
+def create(request):
+    if request.method == 'GET':
+        return render(request, 'create.html')
+    elif request.method == 'POST':
+        url = request.POST['url']
+        code = generate_short_url()
+        new_url = Todo.objects.create(code = code, url = url)
+        new_url.save()
+        return redirect('home')   
+
+def direct(request, id):
+    try:
+        get_url = Todo.objects.get(pk=id)
+    except Todo.DoesNotExist:
+        raise Http404("short url does not exist")
+    return redirect(get_url.url)
+
+
+def detail(request, id):
+    try:
+        task = Todo.objects.get(pk=id)
+    except Todo.DoesNotExist:
+        raise Http404("Task does not exist")
+    return render(request, 'todos/detail.html', {'task': task})
